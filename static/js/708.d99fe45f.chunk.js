3cbb4fa6d174013e0d25c2860b66d4ab817453d3
daria31v <106913534+daria31v@users.noreply.github.com>
2023-03-08 20:28:36 +0000

    Deploying to gh-pages from @ daria31v/goit-react-hw-08-phonebook@a4176ecf788b2817bc72d4bf97a6e9a79c911403 🚀

--- static/js/708.d99fe45f.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[708],{1708:function(e,t,n){n.r(t);var i=n(184),r={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};t.default=function(){return(0,i.jsx)("main",{children:(0,i.jsx)("div",{style:r.container,children:(0,i.jsx)("h1",{style:r.title,children:"Create your Phonebook"})})})}}}]);
-//# sourceMappingURL=708.d99fe45f.chunk.js.map
